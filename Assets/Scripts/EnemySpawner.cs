a743e4a4f1c7f505b3cd86fd937e8cd4484071e0
Bram Willem van der Kuip <b.vanderkuip@ma-web.nl>
2023-09-06 21:54:59 +0200

    Scene 1: WaypointFollower, Path and EnemySpawner

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject enemyPrefab;
+
+    void Start()
+    {
+        StartCoroutine(SpawnEnemy());
+    }
+
+    IEnumerator SpawnEnemy() 
+    {
+        while(true) {
+            Instantiate(enemyPrefab);
+            yield return new WaitForSeconds(1);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,17 @@
+# Tower defense 2D
+
+## Prerequisites
+- Code Editor
+- Unity
+
+## Setup:
+- Open Unity Hub
+- Create "New Project"
+- Select "2D Core"
+- Give your project a name
+- Start with Scene1!
+
+[Scene1](Scene1.md)
+[Scene2](Scene2.md)
+[Scene3](Scene3.md)
+[Scene4](Scene4.md)
